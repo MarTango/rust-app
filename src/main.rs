7b141df889e7178b0dea0a97734104b5217ca55b
Martin Tang <martin.tang365@gmail.com>
2020-06-19 01:48:18 +0100

    Split up app and state into separate modules

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use async_std;
 use std::env;
 
-mod lib;
-use lib::app;
+mod app;
+mod state;
 
 #[async_std::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let app = app();
+    let app = app::app();
 
     let port = env::var("PORT").unwrap_or("8000".to_string());
     app.listen(format!("127.0.0.1:{}", port)).await?;
--- src/state.rs
+++ src/state.rs
@@ -1,19 +1,9 @@
+/// How the fk do i move this to a separate folder
 use std::collections::HashMap;
 
 use async_std::sync::{Arc, Mutex};
-
 use serde_json::Value;
-
-use tide::http::headers::CONTENT_TYPE;
 use tide::http::{Error, StatusCode};
-use tide::{Request, Response};
-
-pub fn app() -> tide::Server<State> {
-    let mut app = tide::with_state(State::new());
-    app.at("/:type").post(post);
-    app.at("/:type/:id").get(get);
-    app
-}
 
 #[derive(Debug)]
 /// In-memory key-Vec store
@@ -26,14 +16,14 @@ pub struct State {
 }
 
 impl State {
-    fn new() -> Self {
+    pub fn new() -> Self {
         State {
             collections: Arc::new(Mutex::new(HashMap::new())),
         }
     }
     /// Insert `payload` into the `typ` collection
     ///
-    async fn insert(&self, typ: &String, payload: Value) -> Result<(), Error> {
+    pub async fn insert(&self, typ: &String, payload: Value) -> Result<(), Error> {
         let payload = match payload {
             Value::Object(x) => Value::Object(x),
             _ => return Err(Error::from_str(StatusCode::BadRequest, "Bad payload")),
@@ -49,7 +39,7 @@ impl State {
     }
 
     /// Get the `id`-th record from the `typ` collection
-    async fn get(&self, typ: &String, id: usize) -> Option<Value> {
+    pub async fn get(&self, typ: &String, id: usize) -> Option<Value> {
         let collections = self.collections.lock().await;
         if let Some(c) = collections.get(typ) {
             if let Some(v) = c.get(id) {
@@ -58,48 +48,6 @@ impl State {
         }
         None
     }
-}
-
-/// Endpoint handler to fetch an entity
-async fn get(req: Request<State>) -> Result<Response, Error> {
-    let typ = req.param("type")?;
-    let id = match req.param::<usize>("id") {
-        Ok(i) => i,
-        Err(_) => return Ok(Response::new(StatusCode::BadRequest)),
-    };
-
-    let stored = match req.state().get(&typ, id).await {
-        Some(thing) => thing,
-        None => {
-            return Err(Error::from_str(
-                StatusCode::NotFound,
-                format!("{} with id {} not found", typ, id),
-            ))
-        }
-    };
-
-    Ok(Response::new(StatusCode::Ok)
-        .append_header(CONTENT_TYPE, "application/json")
-        .body_json(&stored)?)
-}
-
-/// Handler to store a new entity in the state
-async fn post(mut req: Request<State>) -> Result<Response, Error> {
-    let typ = req.param("type")?;
-    let insertion: Value = match req.body_json().await {
-        Ok(Value::Object(i)) => Value::Object(i),
-        _ => {
-            return Ok(Response::new(StatusCode::BadRequest));
-        }
-    };
-
-    let optimistic_response = Response::new(StatusCode::Ok)
-        .append_header(CONTENT_TYPE, "application/json")
-        .body_json(&insertion)?;
-
-    req.state().insert(&typ, insertion).await?;
-
-    Ok(optimistic_response)
 }
 
 #[cfg(test)]
